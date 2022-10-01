e3143f87fe2c2193f8175c7b35b65ff5886bc340
bahrululumfr@gmail.com <bahrululumfr@gmail.com>
2022-10-01 15:23:26 +0700

    first commit

---
+++ Modul 1 (Program Correctness 1)/Kegiatan/src/Tugas1.java
@@ -0,0 +1,11 @@
+public class Tugas1 {
+    public static void main(String[] args) {
+        try{
+            int[] arr = new int[5];
+            arr[4] = 25;
+            System.out.println("Aman");
+        }catch(Exception e){
+            System.out.println("Error");
+        }
+    }
+}
---
+++ Modul 1 (Program Correctness 1)/Kegiatan/src/Tugas2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Tugas2 {
+    static Scanner input = new Scanner(System.in);
+    static String NIM;
+    static boolean ulang = true;
+    //202110370311462
+    public static void menu(){
+        while(ulang){
+            try{
+                System.out.print("Masukan NIM Anda : ");
+                NIM = input.next();
+
+                if(!NIM.contains("10370311")){
+                    throw new Exception("Nim Anda Harus Terdapat 10370311");
+                }else if(NIM.length() > 15){
+                    throw new Exception("NIM lebih dari 15, Coba Lagi");
+                }else if (NIM.length() < 15){
+                    throw new Exception("NIM kurang dari 15, Coba Lagi");
+                } else {
+                    ulang = false;
+                }
+
+                System.out.println("NIM Terinput");
+            }catch(Exception e){
+                System.out.println(e + "\n");
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        menu();
+    }
+}
