e6abc8ede751fbd2602a68696da97919f76cc432
bahrululumfr@gmail.com <bahrululumfr@gmail.com>
2022-10-30 19:00:46 +0700

    Modul

--- Modul 3 (Simple Refactoring)/Kegiatan/src/Balok.java
+++ Modul 3 (Simple Refactoring)/Kegiatan/src/Balok.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Balok {
+public class Balok {
+    private int panjang;
+    private int lebar;
+    private int tinggi;
+
+    public int getPanjang() {
+        return panjang;
+    }
+
+    public void setPanjang(int panjang) {
+        this.panjang = panjang;
+    }
+
+    public int getLebar() {
+        return lebar;
+    }
+
+    public void setLebar(int lebar) {
+        this.lebar = lebar;
+    }
+
+    public int getTinggi() {
+        return tinggi;
+    }
+
+    public void setTinggi(int tinggi) {
+        this.tinggi = tinggi;
+    }
+
+    protected void hasil(){
+        Perhitungan ph = new Perhitungan();
+        System.out.println("Hasil Luas Balok\t : " + ph.luas(panjang, lebar, tinggi) + " cm");
+        System.out.println("Hasil Volume Balok\t : " + ph.volume(panjang, lebar, tinggi) + " cm^3");
+    }
 }
--- Modul 3 (Simple Refactoring)/Kegiatan/src/Driver.java
+++ Modul 3 (Simple Refactoring)/Kegiatan/src/Driver.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Driver {
+import java.util.Scanner;
+
+public class Driver {
+    static int Panjang, Lebar, Tinggi;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Balok balok = new Balok();
+
+        System.out.println("=== Menghitung Luas dan Volume Balok ===\n");
+
+        System.out.print("Masukkan Panjang \t: ");
+        Panjang = input.nextInt();
+        balok.setPanjang(Panjang);
+
+        System.out.print("Masukkan Lebar \t\t: ");
+        Lebar = input.nextInt();
+        balok.setLebar(Lebar);
+
+        System.out.print("Masukkan Tinggi \t: ");
+        Tinggi = input.nextInt();
+        balok.setTinggi(Tinggi);
+
+        System.out.println("\n=== Hasil Perhitungan Balok ===\n");
+        balok.hasil();
+        input.close();
+
+    }
 }
