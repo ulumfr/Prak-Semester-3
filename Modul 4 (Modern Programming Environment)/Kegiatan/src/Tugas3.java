b039883fe04bc1fd9363a29db0843cf927a48e71
bahrululumfr@gmail.com <bahrululumfr@gmail.com>
2022-11-22 07:22:41 +0700

    Modul

--- Modul 4 (Modern Programming Environment)/Kegiatan/src/Tugas3.java
+++ Modul 4 (Modern Programming Environment)/Kegiatan/src/Tugas3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-// Belum Jadi
+// Sudah JADI 
 public class Tugas3 {
 
     static int[] hari = new int[3];
@@ -10,29 +10,37 @@ public class Tugas3 {
 
         Scanner input = new Scanner(System.in);
 
-        //Judul
+        System.out.println("\n===== Modul 4 Proglanjut ====");
+        System.out.println("======= Konversi Hari =======\n");
 
+        System.out.print("Jumlah Hari : ");
+        hari[0] = input.nextInt();
 
-        //Input Konversi
+        setHari(getHari());
+        tahun = konversi(getHari(), 0, 365);
+        bulan = konversi(getHari(), 1, 30);
 
-
-        hari[1] = hari[0];
-
-        //Rumus Konversi
-
-
-        //Output Konversi
-
+        System.out.println(hari[0] + " Hari = " + tahun + " Tahun " + bulan + " Bulan " + hari[2] + " Hari");
         input.close();
     }
 
-//    private static int[] getHari() {
-//        return hari;
-//    }
+    private static int konversi(int[] hari, int x, int x2) {
+        int konversi_hari = hari[x] / x2;
+        hari[x + 1] = hari[x] % x2;
+        return konversi_hari;
+    }
+
+    private static int[] getHari() {
+        return hari;
+    }
+
+    private static void setHari(int[] hari) {
+        Tugas3.hari = hari;
+    }
 }
 
 
-// Sudah JADI 
+// Belum Jadi
 //public class Tugas3 {
 //
 //    static int[] hari = new int[3];
@@ -42,31 +50,23 @@ public class Tugas3 {
 //
 //        Scanner input = new Scanner(System.in);
 //
-//        System.out.println("\n===== Modul 4 Proglanjut ====");
-//        System.out.println("======= Konversi Hari =======\n");
+//        //Judul
 //
-//        System.out.print("Jumlah Hari : ");
-//        hari[0] = input.nextInt();
 //
-//        setHari(getHari());
-//        tahun = konversi(getHari(), 0, 365);
-//        bulan = konversi(getHari(), 1, 30);
+//        //Input Konversi
 //
-//        System.out.println(hari[0] + " Hari = " + tahun + " Tahun " + bulan + " Bulan " + hari[2] + " Hari");
+//
+//        hari[1] = hari[0];
+//
+//        //Rumus Konversi
+//
+//
+//        //Output Konversi
+//
 //        input.close();
 //    }
 //
-//    private static int konversi(int[] hari, int x, int x2) {
-//        int konversi_hari = hari[x] / x2;
-//        hari[x + 1] = hari[x] % x2;
-//        return konversi_hari;
-//    }
-//
-//    private static int[] getHari() {
-//        return hari;
-//    }
-//
-//    private static void setHari(int[] hari) {
-//        Tugas3.hari = hari;
-//    }
+////    private static int[] getHari() {
+////        return hari;
+////    }
 //}
